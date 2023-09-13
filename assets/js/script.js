54e10d20fa8a03d2bb4ced80b592a17b10206257
SolangeI <solange.indaco@gmail.com>
2023-09-12 18:10:29 -0700

    added button per city searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,22 @@ const APIKey = "0dccb660b8ff3b1cd80b2c8036f3c8a3";
 const cityFormEl = document.querySelector('#city-form');
 const cityEl= document.getElementById("city-input");
 const dayForecastListEl = document.querySelectorAll(".day-forecast");
+const searchedCitiesEl = document.getElementById("cities-container");
+
+var cities = [];
+
+function saveCity(cityName){
+  cities.push(cityName);
+  console.log(cities);
+  localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+function appendNewCity(cityName){
+  let cityBtn = document.createElement('button');
+  cityBtn.setAttribute("class", "btn btn-secondary");
+  cityBtn.textContent = cityName;
+  searchedCitiesEl.appendChild(cityBtn);
+}
 
 function displayForecast(data){
   let dayWeatherList = data.list;
@@ -35,11 +51,12 @@ function requestForecast(cityLat, cityLong){
 
   fetch(requestForecasttUrl)
     .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      displayForecast(data);
+      if (response.ok) {
+          response.json().then(function (data) {
+            console.log(data);
+            displayForecast(data);
+        });
+      } else { alert('Error: ' + response.statusText);}
     });
 }
 
@@ -49,25 +66,43 @@ var formSubmitHandler = function requestForecastByCity(event) {
   console.log("City: " + cityName);
 
   if (cityName) {
+    cityLatLon = localStorage.getItem(cityName);
+
+    if (cityLatLon !== null){
+      requestForecast(cityLatLon[0], cityLatLon[1]);
+    }else{
     // Request City coodinates using Geocoding
-    let requestCoodinatesUrl ="http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;
-    fetch(requestCoodinatesUrl)
-      .then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-              requestForecast(data[0].lat, data[0].lon);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      });
-        
-  } else {
+      let requestCoodinatesUrl ="http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;
+      console.log("busco las cordenadas");
+      fetch(requestCoodinatesUrl)
+        .then(function (response) {
+          if (response.ok) {
+              response.json().then(function (data) {
+                requestForecast(data[0].lat, data[0].lon);
+                localStorage.setItem(cityName, [data[0].lat,data[0].lon]);
+                saveCity(cityName);
+                appendNewCity(cityName);
+            });
+          } else { alert('Error: ' + response.statusText);}
+        });
+    }
+  }else {
     alert('Please enter a city name');
   }
-
-  
-
+    
 }
 
 cityFormEl.addEventListener('submit', formSubmitHandler);
+
+function displaySearchedCities(){
+  cities = JSON.parse(localStorage.getItem("cities"));
+  if (cities !== null){
+    for (i=0; i< cities.length; i++){
+      appendNewCity(cities[i]);
+    }
+  }else{
+    cities = [];
+  }
+}
+
+displaySearchedCities();
